fa212fc5f4a75669774f38e7247e2969f320b0bf
the-name-romel <rommelbon100@gmail.com>
2021-10-31 16:23:48 +0800

    Validating the expenses

--- js/app.js
+++ js/app.js
@@ -16,6 +16,23 @@ class HTML{
             budgetTotal.innerHTML = `${amount}`;
             budgetLeft.innerHTML = `${amount}`;
         }
+
+        //Displays a Message Correct or Invalid
+        printMessage(message, className) {
+            const messageWrapper = document.createElement('div');
+            messageWrapper.classList.add('text-center', 'alert', className);
+            messageWrapper.appendChild(document.createTextNode(message));
+
+            //Insert into HTML
+            document.querySelector('.primary').insertBefore(messageWrapper, addExpenseForm);
+            
+            //Clear the Error
+            setTimeout(function() {
+                document.querySelector('.primary .alert').remove();
+                addExpenseForm.reset();
+            }, 3000); 
+        }
+
 }
 
 
@@ -53,5 +70,15 @@ function eventListeners() {
     //When a new expense is added
     addExpenseForm.addEventListener('submit', function(e) {
         e.preventDefault();
+        //Read the input values 
+        const expenseName = document.querySelector('#expense').value;
+        const amount = document.querySelector('#amount').value;
+
+        if(expenseName === '' || amount === '') {
+            html.printMessage('There was an error, all the fields are mandatory', 
+            'alert-danger');
+        } else {
+            console.log('Correct');
+        }
     });
 }
