52b45f8a37036b0448186ce9f89fa5900c4c645b
the-name-romel <rommelbon100@gmail.com>
2021-10-31 16:13:00 +0800

    Getting user budget and creating the classes

--- js/app.js
+++ js/app.js
@@ -1,10 +1,33 @@
 //Classes
 
+class Budget {
+    constructor(budget) {
+        this.budget = Number (budget);
+        this.budgetLeft = this.budget;
+    }
+}
+
+//Everything related to HTML
+class HTML{
+
+        //Insert the budget when the user submits it
+        insertBudget(amount) {
+            //Insert to the HTML
+            budgetTotal.innerHTML = `${amount}`;
+            budgetLeft.innerHTML = `${amount}`;
+        }
+}
+
 
 //Variables
-const addExpenseForm = document.querySelector('#add-expense');
+const addExpenseForm = document.querySelector('#add-expense'),
+      budgetTotal = document.querySelector('span#total'),
+      budgetLeft = document.querySelector('span#left');
 
 let budget, userBudget;
+
+//Instanciate the HTML Class
+const html = new HTML();
 
 //EventListeners
 eventListeners();
@@ -17,11 +40,18 @@ function eventListeners() {
         //Validate the user budget
         if(userBudget === null || userBudget === '' || userBudget === '0') {
             window.location.reload();
+        } else {
+            //Budget is valid then intanciate budget class
+            budget = new Budget(userBudget);
+            
+            //Instanciate the HTML Class
+            html.insertBudget(budget.budget);
         }
+            
     });
 
     //When a new expense is added
-    addExpenseForm.eventListeners('submit', function(e) {
+    addExpenseForm.addEventListener('submit', function(e) {
         e.preventDefault();
     });
 }
